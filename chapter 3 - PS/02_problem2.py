1aa0e9ead6639614e2e90099eb6a0746d074e6f7
shivanimahar <shivanimahar.offc@gmail.com>
2025-06-03 09:02:05 +0530

    chapter3 - Ps

---
+++ chapter 3 - PS/02_problem2.py
@@ -0,0 +1,5 @@
+letter = '''Dear <|Name|>,
+You are selected! 
+<|Date|>'''
+
+print(letter.replace("<|Name|>","shivani").replace("<|Date|", "19 dec 2025"))
---
+++ chapter 3 - PS/05_problem5.py
@@ -0,0 +1,3 @@
+letter = "Dear haryy, \n\tThis python course is nice. \nThanks!"
+
+print(letter)
