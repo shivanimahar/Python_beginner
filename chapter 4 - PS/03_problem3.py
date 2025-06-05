5df8d429813a586b335f4427a2ee073b2696bec2
shivanimahar <shivanimahar.offc@gmail.com>
2025-06-05 16:41:38 +0530

    chapter4 - Ps

---
+++ chapter 4 - PS/03_problem3.py
@@ -0,0 +1,4 @@
+# Check that a tuple type cannot be changed in python.
+a = (23, 34, "Harry")
+
+a[2] = "Jerry"
---
+++ chapter 4 - PS/04_problem4.py
@@ -0,0 +1,4 @@
+# Write a program to sum a list with 4 numbers.
+l = [65, 65, 87, 98]
+
+print(sum(l))
