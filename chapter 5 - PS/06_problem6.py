427f175fc4f4308a89691504aa351becc8cb792e
shivanimahar <shivanimahar.offc@gmail.com>
2025-06-07 12:48:06 +0530

    chapter5 - Ps

---
+++ chapter 5 - PS/06_problem6.py
@@ -0,0 +1,20 @@
+# Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique.
+d = {}
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+print(d)
---
+++ chapter 5 - PS/08_problem8.py
@@ -0,0 +1,4 @@
+# Can you change the values inside a list which is contained in set S? s = {8, 7, 12, "Harry", [1,2]} 
+s = {8, 7, 12, "Harry", [1,2]}
+
+# no, you cannot change the values inside a list that is inside a set, because  lists are mutable (i.e. they can be changed), and sets only allow immutable (unchangeable) items.
