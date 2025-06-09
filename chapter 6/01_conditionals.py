58cde7b1cbd7100930d75f0c839860aeffe95568
shivanimahar <shivanimahar.offc@gmail.com>
2025-06-09 22:55:12 +0530

    modified

--- chapter 6/01_conditionals.py
+++ chapter 6/01_conditionals.py
@@ -4,6 +4,9 @@ if(a>=18):
     print("You are above the age of consent")
     print("Good for you")
 
+elif(a<0):
+    print("You are entering an invalid age")
+    
 else:
     print("You are below the age of consent")
 
