e9ee7065f565f06f986bebe14e300a87cc72dd6d
shivanimahar <shivanimahar.offc@gmail.com>
2025-06-15 22:40:38 +0530

    ch6.ps

---
+++ chapter 6 - PS/05_problem5.py
@@ -0,0 +1,1 @@
+# Write a program which finds out whether a given name is present in a list or not
---
+++ chapter 6 - PS/06_problem6.py
@@ -0,0 +1,1 @@
+# Write a program to calculate the grade of a student from his marks from the following scheme
