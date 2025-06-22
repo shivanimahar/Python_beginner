7ab57e849ed0363e822844de6d623e4a4b238543
shivanimahar <shivanimahar.offc@gmail.com>
2025-06-22 13:37:22 +0530

    ch-7

---
+++ chapter 7/03_list_using_while.py
@@ -0,0 +1,9 @@
+l = [1, "Harry", False, "Shivani", "rohan", "shubam", "shubhi"]
+
+i =  0
+
+while(i<len(l)):
+    print(l[i])
+    i +=1
+
+    
---
+++ chapter 7/05_for_loop_iterate.py
@@ -0,0 +1,15 @@
+l = [1, 4, 6, 234, 6, 764]
+
+# for loops with lists
+for i in l:
+    print(i)
+
+#for loops with tuples
+t = (6, 231, 75, 122)
+for i in t:
+    print(i)
+
+# for loops with strings
+s = "Harry"
+for i in s:
+    print(i)
