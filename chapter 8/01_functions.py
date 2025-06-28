16414576a6e3ed02a58ff534f4153ec6dadcce20
shivanimahar <shivanimahar.offc@gmail.com>
2025-06-28 22:33:56 +0530

    ch-8

---
+++ chapter 8/01_functions.py
@@ -0,0 +1,35 @@
+# a = int(input("Enter number: "))More actions
+# b = int(input("Enter number: "))
+# c = int(input("Enter number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+
+# a = int(input("Enter number: "))
+# b = int(input("Enter number: "))
+# c = int(input("Enter number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+# Funtion definition
+def avg():
+   a = int(input("Enter number: "))
+   b = int(input("Enter number: "))
+   c = int(input("Enter number: "))
+
+   average = (a+b+c)/3
+   print(average)
+
+avg() # Function call
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+
