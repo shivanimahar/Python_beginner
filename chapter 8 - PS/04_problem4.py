b57cd0870e21375537e7ebef073d10f3ef971032
shivanimahar <shivanimahar.offc@gmail.com>
2025-07-06 22:24:12 +0530

    ch-8 ps completed

--- chapter 8 - PS/04_problem4.py
+++ chapter 8 - PS/04_problem4.py
@@ -1,1 +1,17 @@
 # Write a recursive function to calculate the sum of first n natural numbers.
+'''
+sum(1) = 1
+sum(2) = 1 + 2
+sum(3) = 1+ 2 + 3
+
+sum(n) = 1 + 2 +...+ n
+sum(n) = sum(n-1) + n
+
+'''
+
+def sum(n):
+    if(n==1):
+        return 1
+    return sum(n-1) + n
+
+print(sum(4))
--- chapter 8 - PS/05_problem5.py
+++ chapter 8 - PS/05_problem5.py
@@ -1,1 +1,8 @@
 # Write a python function to print first n lines of the following pattern:
+def pattern(n):
+    if(n==0):
+        return
+    print("*" * n)
+    pattern(n-1)
+
+pattern(3)
