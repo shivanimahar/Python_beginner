c8d0c192e00673b6c3d44d18a4d65ea8aebeceaf
shivanimahar <shivanimahar.offc@gmail.com>
2025-07-10 14:41:25 +0530

    ch9-Ps completed

--- chapter 9 - PS/09_problem9.py
+++ chapter 9 - PS/09_problem9.py
@@ -1,1 +1,12 @@
 # Write a program to find out whether a file is identical & matches the content of another file.
+with open("file.txt") as f:
+    content1 = f.read()
+
+with open("poem.txt") as f:
+    content2 = f.read()
+
+if(content1 == content2):
+    print("Yes these files are identical")
+
+else:
+    print("No these files are not identical")
