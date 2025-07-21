f89acd1cef237e9926af05ad8155063c647f0e8c
shivanimahar <shivanimahar.offc@gmail.com>
2025-07-21 23:30:00 +0530

    ch = 11

--- chapter 11/01_inheritance.py
+++ chapter 11/01_inheritance.py
@@ -1,4 +1,18 @@
 class Employee:
+    company = "ITC"
+    def show(self):
+        print(f"The name of the Employee is {self.name} and the salary is {self.salary}")
+
+
+class Programmer:
+    company = "ITC InfoTech"
     def show(self):
         print(f"The name is {self.name} and the salary is {self.salary}")
-print()
+
+    def showLanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language")
+
+a = Employee()
+b = Programmer()
+
+print(a.company, b.company)
