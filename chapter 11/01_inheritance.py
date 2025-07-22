a78b3329ce3184caba16d4017fc0b8ebd56b5ea4
shivanimahar <shivanimahar.offc@gmail.com>
2025-07-22 23:42:56 +0530

    ch - 11

--- chapter 11/01_inheritance.py
+++ chapter 11/01_inheritance.py
@@ -4,11 +4,17 @@ class Employee:
         print(f"The name of the Employee is {self.name} and the salary is {self.salary}")
 
 
-class Programmer:
-    company = "ITC InfoTech"
-    def show(self):
-        print(f"The name is {self.name} and the salary is {self.salary}")
+# class Programmer:
+#     company = "ITC Infotech"
+#     def show(self):
+#         print(f"The name is {self.name} and the salary is {self.salary}")
 
+#     def showLanguage(self):
+#         print(f"The name is {self.name} and he is good with {self.language} language")
+
+
+class Programmer(Employee):
+    company = "ITC Infotech"
     def showLanguage(self):
         print(f"The name is {self.name} and he is good with {self.language} language")
 
