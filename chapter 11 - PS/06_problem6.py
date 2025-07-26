c28cf7417cb61e913383fc18e48033fcf1067823
shivanimahar <shivanimahar.offc@gmail.com>
2025-07-26 23:30:06 +0530

    ch 11 Ps

---
+++ chapter 11 - PS/06_problem6.py
@@ -0,0 +1,2 @@
+# 6. Write __str__() method to print the vector as follows: 7i + 8j +10k
+# Assume vector of dimension 3 for this problem.
---
+++ chapter 11 - PS/07_problem7.py
@@ -0,0 +1,1 @@
+# 7. Override the __len__() method on vector of problem 5 to display the dimension of the vector.
