8aa755ac9025c81b2cf169e80a74ec44c59167aa
shivanimahar <shivanimahar.offc@gmail.com>
2025-07-31 22:38:08 +0530

    chapter 12

---
+++ chapter 12/02_types.py
@@ -0,0 +1,13 @@
+from typing import List, Tuple, Union
+
+
+n : int = 5
+
+name: str = "Harry"
+
+
+def sum(a: int, b: int) -> int:
+    return a + b
+
+
+
---
+++ chapter 12/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+print(http_status(404)) # Output: Not Found
+# print(http_status(500)) # Output: Internal Server Error
+# print(http_status(403)) # Output: Unknown status
