4530b57ed8e0bd61aacabb8ed83922c4baf846b2
shivanimahar <shivanimahar.offc@gmail.com>
2025-08-01 23:00:19 +0530

    ch-12

---
+++ chapter 12/04_exception.py
@@ -0,0 +1,13 @@
+try:
+    a = int(input("Hey, Enter a number: "))
+    print(a)
+
+except ValueError as v:
+    print("Heyyyy")
+    print(v)
+
+except Exception as e:
+    print(e)
+
+print("Thank you")
+        
--- chapter 12/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-print(http_status(404)) # Output: Not Found
-# print(http_status(500)) # Output: Internal Server Error
-# print(http_status(403)) # Output: Unknown status
