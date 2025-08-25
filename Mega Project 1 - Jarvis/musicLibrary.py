2b35efeaacf94259e4a7e01ed9880d30eaff569d
shivanimahar <shivanimahar.offc@gmail.com>
2025-08-25 22:51:31 +0530

    Mega Project-1 Jarvis completed

---
+++ Mega Project 1 - Jarvis/musicLibrary.py
@@ -0,0 +1,12 @@
+music = {
+    "Jhol" : "https://www.youtube.com/shorts/JEZkiHb-KqA",
+    "CO2" : "https://www.youtube.com/watch?v=U2SVCCENLjE&list=RDU2SVCCENLjE&start_radio=1&pp=ygUDY28yoAcB",
+    "Finding her" : "https://youtu.be/3Cp2QTBZAFQ?list=RDU2SVCCENLjE",
+    "Jo tum mere ho" : "https://www.youtube.com/shorts/VqBFl9qVGd8",
+    "The night we met" : "https://youtu.be/KtlgYxa6BMU?list=RDU2SVCCENLjE",
+    "Perfect" : "https://youtu.be/2Vv-BfVoq4g?list=RDU2SVCCENLjE",
+    "I wanna be yours" : "https://youtu.be/pQbvrDl3kcY?list=RDU2SVCCENLjE",
+    "Dandelions" : "https://youtu.be/5gg17XXXiNo?list=RDU2SVCCENLjE",
+    "Teenage dreams" : "https://youtu.be/pVi9W3OThVw?list=RDU2SVCCENLjE",
+    "Blue" : "https://youtu.be/IpFX2vq8HKw?list=RDpVi9W3OThVw"
+}
