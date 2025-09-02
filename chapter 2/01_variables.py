6f0aa405f962eca7aaf951fe6e22b5aff1a4f1c3
shivanimahar <shivanimahar.offc@gmail.com>
2025-09-02 20:27:10 +0530

    chapter - 2

--- chapter 2/01_variables.py
+++ chapter 2/01_variables.py
@@ -7,3 +7,6 @@ c = 7
 name = "harry"
 
 print(a + b)
+
+# here a, b, c, name are variables
+# and 1,2,7 are values
--- chapter 2/04_operators.py
+++ chapter 2/04_operators.py
@@ -2,26 +2,30 @@
 a = 4
 b = 4
 c = a + b 
-print( c )
+print(c)
+
 
 # Assignment operators
 a = 4-2 # Assign 4-2 in a
 print(a)
 b = 6
 # b += 3 # Increment the value of b by 3 and then assign it to b
-b -= 3 # Decrement the value of b by 3 and then assign it to b
+# b -= 3 # Decrement the value of b by 3 and then assign it to b
+# print(b)
+b *= 3
 print(b)
 
-# Comparison Operators
+# Comparison Operators # they always gives boolean(T/F) value
+# NOTE = (=) is assignment operator, and (==) is a comparison operator
 
-d = 5==5
-
+d = 5==5 # It checks if 5 is equal to 5
+# d = 5 != 7 # it means 5 is not equals to 7
 print(d)
-
 
 # Logical Operators
 
 e = True or False
+print(e)
 
 # Truth table of 'or'
 print("True or False is ", True or False)
@@ -36,4 +40,4 @@ print("True and True is ", True and True)
 print("False and True is ", False and True)
 print("False and False is ", False and False)
 
-print(not(True))
+print(not(True)) # not(works on only single operant) = T ko F kardeta hai, or F ko T kar deta hai
