d854b52decd8f74e67ff5ae94f0552704dca0667
shivanimahar <shivanimahar.offc@gmail.com>
2025-09-09 22:28:00 +0530

    rev

--- Python Revsion(1)/03_problem3(01).py
+++ Python Revsion(1)/03_problem3(01).py
@@ -1,2 +1,1 @@
-# 3. Install an external module and use it to perform an operation of your interest.  
-pip install
+# 3. Install an external module and use it to perform an operation of your interest.
---
+++ Python Revsion(1)/09_problem4(02).py
@@ -0,0 +1,7 @@
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than 
+# ‘b’ or not. Take a = 34 and b = 80 
+a = int(input("Enter the value of a: "))
+b = int(input("Enter the value of b: "))
+print("a is greater than b: ", a>b)
+
+
