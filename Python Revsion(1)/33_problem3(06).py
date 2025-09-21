4153999151da386a4e1c850de140e0f63361bb67
shivanimahar <shivanimahar.offc@gmail.com>
2025-09-21 20:27:54 +0530

    rev

--- Python Revsion(1)/33_problem3(06).py
+++ Python Revsion(1)/33_problem3(06).py
@@ -1,3 +1,4 @@
-3. A spam comment is defined as a text containing following keywords: 
-“Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program 
-to detect these spams. 
+# 3. A spam comment is defined as a text containing following keywords: “Make a lot of money”, “buy now”, “subscribe this”,
+#  “click this”.Write a program to detect these spams. 
+ 
+
